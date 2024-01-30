d05ebb859f915db47110d363d61e66c78cff1e33
suraj <developersurajkumar1840@gmail.com>
2024-01-30 17:06:14 +0530

    30/01/2024 day commit

--- react-spin/src/main.jsx
+++ react-spin/src/main.jsx
@@ -4,9 +4,10 @@ import App from './App.jsx'
 import './index.css'
 import App2 from './App2.jsx'
 import Refresh from './Refresh.jsx'
+import Mainspin from './components/Mainspin.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App2/>
+    <Mainspin/>
   </React.StrictMode>,
 )
